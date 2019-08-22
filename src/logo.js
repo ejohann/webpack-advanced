b78c680d98c61666a254e98e79b7ab51c7ae6c6c
Johanne <johanne_lewis@hotmail.com>
2019-08-22 12:14:46 +0100

    Import image class, instantiate and render image

--- src/logo.js
+++ src/logo.js
@@ -1,5 +1,9 @@
-import Heading from './components/heading/heading.js';
+import Heading from './components/heading/heading';
+import Hdlogo from './hd-logo/hd-logo';
 
 
 const heading = new Heading();
 header.render();
+
+const hdlogo = new Hdlogo();
+hdlogo.render();
